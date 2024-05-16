7b6ddab486096d5878d9a0041213cba71ab64621
88xxd <150152512+88xxd@users.noreply.github.com>
2024-05-16 21:33:58 +0800

    元始版本

---
+++ ProjectVR/Intermediate/Build/Win64/x64/ProjectVR/Development/Core/SharedPCH.Core.ShadowErrors.InclOrderUnreal5_0.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.38.33134
+#include "C:/Users/肖湘东xxd/Documents/Unreal Projects/ProjectVR/Intermediate/Build/Win64/x64/ProjectVR/Development/Core/SharedPCH.Core.ShadowErrors.InclOrderUnreal5_0.h"
---
+++ ProjectVR/Intermediate/Source/ProjectVR.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class ProjectVR : ModuleRules
+{
+	public ProjectVR(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
